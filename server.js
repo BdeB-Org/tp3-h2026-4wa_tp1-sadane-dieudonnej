0da37e0cd805b25e879e6ba351f380ffcd81f3cc
nen12 <6273595@bdeb.qc.ca>
2026-04-30 20:45:52 -0400

    Création des dossiers html, css et js et installation du cors

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+
+// Permet de référencer le Cross-Origin Resource Sharing
+const cors = require("cors");
+app.use(cors());
+
+//Lignes de code pour les routes.
 const projetRoutes = require("./routes/projetRoutes");
 const etudiantRoutes = require("./routes/etudiantRoutes");
+
+//Lignes de code pour les app
 app.use(express.json());
 app.use("/", projetRoutes);
 app.use ("/", etudiantRoutes);
