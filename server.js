f32aadf5a61bc34f189474db7f7cb97855083e2e
nen12 <6273595@bdeb.qc.ca>
2026-05-02 01:06:00 -0400

    ajout d'un rangé mot de passe

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const projetRoutes = require("./routes/projetRoutes");
 const etudiantRoutes = require("./routes/etudiantRoutes");
-
+//Ajout de cors
+const cors = require("cors");
+//Ligne de codes app
 app.use(express.json());
 app.use("/", projetRoutes);
 app.use ("/", etudiantRoutes);
+//app cors
+app.use(cors());
+
+//Ligne de code serveur
 app.listen(3000, () => {
 console.log("Serveur lancé sur http://localhost:3000");
 });
